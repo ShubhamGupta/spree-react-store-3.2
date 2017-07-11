51c3fa638a115727fb8f314857d270255598a824
Shubham Gupta <shubham@vinsol.com>
2017-07-11 19:41:09 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## README
+
+This is a sample Rails-5.0.4 application. It serves as an example API backend required to run [spree-on-react](github.com/vinsol-spree-contrib/spree-on-react).
+
+Rails 5.0
+
+Spree (3.2)
+
+Spree AMS
+
+CORS enabled
+
+Run this app on port specified in .env file in spree-on-react project. This project expects spree-on-react server to be running on loalhost:3000. You may tweak this behaviour in configurations.
---
+++ config/application.rb
@@ -0,0 +1,35 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SpreeReactStore32
+  class Application < Rails::Application
+    
+    config.to_prepare do
+      # Load application's model / class decorators
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+
+      # Load application's view overrides
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+    
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
+      end
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
